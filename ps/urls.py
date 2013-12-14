78d88cc9b06f03d43573cdd8916d4d19c85059fa
Teddy <i.kenting@gmail.com>
2013-12-14 23:42:09 +0800

    添加UT，在首页显示HTML内容

--- ps/urls.py
+++ ps/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ps.views.home', name='home'),
+    url(r'^$', 'parents.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
